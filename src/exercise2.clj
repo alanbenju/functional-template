1e707d1121c8d26a453251f7aa415f07962781b6
alanbenju <alanbenju@hotmail.com>
2017-08-21 16:41:01 -0300

    Exercise 2,3,4 tp1

---
+++ src/exercise2.clj
@@ -0,0 +1,9 @@
+(ns exercise2)
+
+(defn a [x] (< 5 x))
+(defn only-greater-than-five [x]
+  (filter a x))
+
+
+
+
---
+++ src/exercise3.clj
@@ -0,0 +1,3 @@
+(ns exercise3)
+(defn fibonacci [x]	
+	(cond (= 0 x) 0 (= 1 x) 1 :else ( + (fibonacci (- x 1)) (fibonacci (- x 2)) )))
